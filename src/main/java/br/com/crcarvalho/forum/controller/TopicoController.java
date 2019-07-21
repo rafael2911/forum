1ad66159c25983ed2457f7f1ed9a1f8f0296278a
Rafael Carvalho <crcarvalho@outlook.com>
2019-07-21 13:18:36 -0300

    Ordenação

--- src/main/java/br/com/crcarvalho/forum/controller/TopicoController.java
+++ src/main/java/br/com/crcarvalho/forum/controller/TopicoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,9 +43,9 @@ public class TopicoController {
 	
 	@GetMapping
 	public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,
-				@RequestParam int pagina, @RequestParam int qtd){
+				@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao){
 		
-		Pageable paginacao = PageRequest.of(pagina, qtd);
+		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
 		
 		if(nomeCurso == null)
 			return TopicoDto.converter(topicoRepository.findAll(paginacao));
