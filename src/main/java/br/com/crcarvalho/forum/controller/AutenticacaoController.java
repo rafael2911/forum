6d6de8a12eaecddb57f61847c0a06ed329f5d894
Rafael Carvalho <crcarvalho@outlook.com>
2019-07-24 12:08:34 -0300

    Retornando o token para o cliente

--- src/main/java/br/com/crcarvalho/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/crcarvalho/forum/controller/AutenticacaoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.crcarvalho.forum.config.security.TokenService;
+import br.com.crcarvalho.forum.controller.dto.TokenDto;
 import br.com.crcarvalho.forum.controller.form.LoginForm;
 
 @RestController
@@ -27,7 +28,7 @@ public class AutenticacaoController {
 	private TokenService tokenService;
 	
 	@PostMapping
-	public ResponseEntity<Void> autenticar(@RequestBody @Valid LoginForm form){
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form){
 		
 		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 		
@@ -39,7 +40,7 @@ public class AutenticacaoController {
 			String token = tokenService.gerarToken(authentication);
 			System.out.println(token);
 			
-			return ResponseEntity.ok().build();
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 		}catch (AuthenticationException ex) {
 			return ResponseEntity.badRequest().build();
 		}
