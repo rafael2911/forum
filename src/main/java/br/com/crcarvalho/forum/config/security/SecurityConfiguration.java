b969a54c230722fe6ea6239b0540d3b7089ce02d
Rafael Carvalho <crcarvalho@outlook.com>
2019-07-24 22:10:32 -0300

    Documentação da API com SpringFox Swagger

--- src/main/java/br/com/crcarvalho/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/crcarvalho/forum/config/security/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	// configurações de arquivos estáticos
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 
 	@Override
---
+++ src/main/java/br/com/crcarvalho/forum/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,25 @@
+package br.com.crcarvalho.forum.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.crcarvalho.forum.model.Usuario;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+	
+	@Bean
+	public Docket forumAPI() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.crcarvalho.forum"))
+				.paths(PathSelectors.ant("/**"))
+				.build()
+				.ignoredParameterTypes(Usuario.class);
+	}
+	
+}
